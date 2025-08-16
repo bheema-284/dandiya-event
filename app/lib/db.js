ac66d4e1480fbee8bb07c3854292f63e43aac3c2
bheema-284 <bheemudug@gmail.com>
2025-08-16 22:30:01 +0530

    refactor: handle user details

--- app/lib/db.js
+++ app/lib/db.js
@@ -2,23 +2,27 @@
 import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI;
-const options = {};
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+if (!uri) {
+    throw new Error("Please add your Mongo URI to .env.local");
+}
 
 let client;
 let clientPromise;
 
-if (!process.env.MONGODB_URI) {
-    throw new Error("Please add your Mongo URI to .env.local");
-}
-
-if (process.env.NODE_ENV === "local") {
-    // Use a global var so we don’t create multiple connections
-    if (!global._mongoClientPromise) {
+if (process.env.NODE_ENV === "development") {
+    // Use global variable to prevent multiple connections in development
+    if (!globalThis._mongoClientPromise) {
         client = new MongoClient(uri, options);
-        global._mongoClientPromise = client.connect();
+        globalThis._mongoClientPromise = client.connect();
     }
-    clientPromise = global._mongoClientPromise;
+    clientPromise = globalThis._mongoClientPromise;
 } else {
+    // In production, create a new client
     client = new MongoClient(uri, options);
     clientPromise = client.connect();
 }
